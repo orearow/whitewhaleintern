41460f252dfaa049301728fcfed85cb615d8199f
orearow <oreoluwa.arowobusoye@ucalgary.ca>
2017-04-27 12:58:03 -0500

    changed a few comments

--- gibgen.java
+++ gibgen.java
@@ -11,7 +11,8 @@ public class gibgen {
     if (args.length == 0) {
             System.out.println("no arguments were given.");
         }
-//testing to see if the command lind input is the name of a file or just some string
+//testing to see if the command ine input is the name of a text file or just some string
+
     String input= args[0];
     if (input.substring(input.length() - 3,input.length() ).equals("txt")){
     // reading the file         
@@ -29,9 +30,11 @@ public class gibgen {
                 catch(Exception e){
                   System.out.println( input+ " file not found");
                 }
-              }
+    }
+//command line input will be treated as a string 
     else{          
-     str= input;}
+     str= input;
+  }
 
 
 //splitting the string at whitespace and punctuation
@@ -46,44 +49,43 @@ String[] splitArray = str.split("\\s+|(?=\\p{Punct})|(?<=\\p{Punct})");
     }
 
        newText= (newText + " "+ word);
-      }
+  }
       //getting rid of the extra white space before the punctuation
-      newText = newText.replaceAll("\\s+(?=\\p{Punct})", "");
+  newText = newText.replaceAll("\\s+(?=\\p{Punct})", "");
 
-      System.out.println(newText);
+  System.out.println(newText);
 
   }
 
 
 public static String shuffle(String word){
 //storing the first and last letters of the word
-String firstLetter = word.substring(0, 1);
-String lastLetter=word.substring(word.length() - 1);
+  String firstLetter = word.substring(0, 1);
+  String lastLetter=word.substring(word.length() - 1);
 
-//substring contains remaining words to be scrambled 
-String remaining =word.substring(1, word.length() - 1);
+  //substring contains remaining words to be scrambled 
+  String remaining =word.substring(1, word.length() - 1);
 
-//scrambling the remaining character in the array
-char[] characters =word.substring(1, word.length() - 1).toCharArray();
+  //scrambling the remaining character in the array
+  char[] characters =word.substring(1, word.length() - 1).toCharArray();
 
     for (int i =0; i < characters.length; i++) {
-        int randomIndex = (int)(Math.random() * characters.length);
+      int randomIndex = (int)(Math.random() * characters.length);
+      char temp = characters[i];
+      characters[i] = characters[randomIndex];
+      characters[randomIndex] = temp;
+      }
+      //concatinating the first and last word with the scrambled bits to create the readable scrambled word
+      String newWord= (firstLetter+ new String(characters) + lastLetter);
+      //checking to make sure that the word has been scrambled because random sort of sucks, checks if remaining substring letters 
+      //are the same since they can not be scrambled 
+      if ((newWord.equals(word)) &&(!remaining.equals(new StringBuilder(remaining).reverse().toString()) )) {
+        //rescrable if the word still isn't scrambled
+        newWord=shuffle(word);
 
-        char temp = characters[i];
-        characters[i] = characters[randomIndex];
-        characters[randomIndex] = temp;
-    }
-    //concatinating the first and last word with the scrambled bits to create the readable scrambled word
-    String newWord= (firstLetter+ new String(characters) + lastLetter);
-    //checking to make sure that the word has been scrambled because random sort of sucks, checks if remaining substring is a palendrome
-    //since palendromes cant ve scrambled
-    if ((newWord.equals(word)) &&(!remaining.equals(new StringBuilder(remaining).reverse().toString()) )) {
+  }
+      return new String(newWord);
 
-      newWord=shuffle(word);
-
-}
-    return new String(newWord);
-
-}
+  }
 
 }   
